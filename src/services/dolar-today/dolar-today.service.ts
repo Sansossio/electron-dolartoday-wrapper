22cdc104b3d629a96914729efab2f8e1dd49d2ef
Sansossio <juliosansossio@gmail.com>
2020-04-17 17:24:24 +0200

    remove magic string

--- src/services/dolar-today/dolar-today.service.ts
+++ src/services/dolar-today/dolar-today.service.ts
@@ -3,12 +3,13 @@ import { DolarTodayDto } from './dto/dolar-today.dto'
 import { dolarTodayMapper } from './mapper/dolar-today.mapper'
 
 const DATA_URL = 'https://s3.amazonaws.com/dolartoday/data.json'
+const method = 'GET'
 
 export class DolarTodayService {
   static async getData (): Promise<DolarTodayDto> {
     const { data } = await axios({
       url: DATA_URL,
-      method: 'GET'
+      method
     })
     return dolarTodayMapper(data)
   }
